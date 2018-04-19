747561f46e81be084562cb0c057e2b789efed1f9
Vektor Dewanto <vektor.dewanto@gmail.com>
2018-04-19 10:59:40 +1000

    add main()

--- ch09/05_policy_gradients_pong.py
+++ ch09/05_policy_gradients_pong.py
@@ -5,109 +5,17 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from gym import wrappers
 
-class PolicyNetwork(object):
-    def __init__(self, N_SIZE, h=200, gamma=0.99, eta=1e-3, decay=0.99, save_path = 'models2/pong.ckpt' ):
-
-        self.gamma = gamma
-        self.save_path = save_path
-        # Placeholders for passing state....
-        self.tf_x = tf.placeholder(dtype=tf.float32, shape=[None, N_SIZE * N_SIZE], name="tf_x")
-        self.tf_y = tf.placeholder(dtype=tf.float32, shape=[None, n_actions], name="tf_y")
-        self.tf_epr = tf.placeholder(dtype=tf.float32, shape=[None, 1], name="tf_epr")
-
-        # Weights
-        xavier_l1 = tf.truncated_normal_initializer(mean=0, stddev=1. / N_SIZE, dtype=tf.float32)
-        self.W1 = tf.get_variable("W1", [N_SIZE * N_SIZE, h], initializer=xavier_l1)
-        xavier_l2 = tf.truncated_normal_initializer(mean=0, stddev=1. / np.sqrt(h), dtype=tf.float32)
-        self.W2 = tf.get_variable("W2", [h, n_actions], initializer=xavier_l2)
-
-        # Build Computation
-        # tf reward processing (need tf_discounted_epr for policy gradient wizardry)
-        tf_discounted_epr = self.tf_discount_rewards(self.tf_epr)
-        tf_mean, tf_variance = tf.nn.moments(tf_discounted_epr, [0], shift=None, name="reward_moments")
-        tf_discounted_epr -= tf_mean
-        tf_discounted_epr /= tf.sqrt(tf_variance + 1e-6)
-
-        # Define Optimizer, compute and apply gradients
-        self.tf_aprob = self.tf_policy_forward(self.tf_x)
-        loss = tf.nn.l2_loss(self.tf_y - self.tf_aprob)
-        optimizer = tf.train.RMSPropOptimizer(eta, decay=decay)
-        tf_grads = optimizer.compute_gradients(loss, var_list=tf.trainable_variables(), grad_loss=tf_discounted_epr)
-        self.train_op = optimizer.apply_gradients(tf_grads)
-
-        # Initialize Variable
-        init = tf.global_variables_initializer()
-
-        self.session = tf.InteractiveSession()
-        self.session.run(init)
-        self.load()
-
-
-    def tf_discount_rewards(self, tf_r):  # tf_r ~ [game_steps,1]
-        discount_f = lambda a, v: a * self.gamma + v;
-        tf_r_reverse = tf.scan(discount_f, tf.reverse(tf_r, [0]))
-        tf_discounted_r = tf.reverse(tf_r_reverse, [0])
-        return tf_discounted_r
-
-    def tf_policy_forward(self, x): #x ~ [1,D]
-         h = tf.matmul(x, self.W1)
-         h = tf.nn.relu(h)
-         logp = tf.matmul(h, self.W2)
-         p = tf.nn.softmax(logp)
-         return p
-
-    def update(self, feed):
-        return self.session.run(self.train_op, feed)
-
-    def load(self):
-        self.saver = tf.train.Saver(tf.global_variables())
-        load_was_success = True  # yes, I'm being optimistic
-        try:
-            save_dir = '/'.join(self.save_path.split('/')[:-1])
-            ckpt = tf.train.get_checkpoint_state(save_dir)
-            load_path = ckpt.model_checkpoint_path
-            self.saver.restore(self.session, load_path)
-        except:
-            print("no saved model to load. starting new session")
-            load_was_success = False
-        else:
-            print("loaded model: {}".format(load_path))
-            saver = tf.train.Saver(tf.global_variables())
-            episode_number = int(load_path.split('-')[-1])
-
-    def save(self):
-        self.saver.save(self.session, self.save_path, global_step=n)
-        print("SAVED MODEL #{}".format(n))
-
-
-    def predict_UP(self,x):
-        feed = {self.tf_x: np.reshape(x, (1, -1))}
-        aprob = self.session.run(self.tf_aprob, feed);
-        return aprob
-
-
-# downsampling
-def preprocess(I):
-    """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-    I = I[35:195] # crop
-    I = I[::2,::2,0] # downsample by factor of 2
-    I[I == 144] = 0  # erase background (background type 1)
-    I[I == 109] = 0  # erase background (background type 2)
-    I[I != 0] = 1    # everything else (paddles, ball) just set to 1
-    return I.astype(np.float).ravel()
-
-
-if __name__ == '__main__':
+def main()
     # Create Game Environment
     env_name = "Pong-v0"
     env = gym.make(env_name)
-    env = wrappers.Monitor(env, '/tmp/pong', force=True)
+    env = wrappers.Monitor(env, './log/pong', force=True)
     n_actions = env.action_space.n  # Number of possible actions
+
     # Initializing Game and State(t-1), action, reward, state(t)
     xs, rs, ys = [], [], []
     obs = env.reset()
     prev_x = None
-
 
     running_reward = None
     running_rewards = []
@@ -170,3 +78,103 @@ if __name__ == '__main__':
     plt.ylabel('Running Averge')
     plt.show()
     env.close()
+
+class PolicyNetwork(object):
+    def __init__(self, N_SIZE, h=200, gamma=0.99, eta=1e-3, decay=0.99, save_path = 'models2/pong.ckpt' ):
+        self.gamma = gamma
+        self.save_path = save_path
+
+        # Placeholders for passing state....
+        # input state, self.tf_x;
+        # predicted action, self.tf.y;
+        # corresponding reward, self.tf_epr;
+        self.tf_x = tf.placeholder(dtype=tf.float32, shape=[None, N_SIZE * N_SIZE], name="tf_x")
+        self.tf_y = tf.placeholder(dtype=tf.float32, shape=[None, n_actions], name="tf_y")
+        self.tf_epr = tf.placeholder(dtype=tf.float32, shape=[None, 1], name="tf_epr")
+
+        # Weights
+        xavier_l1 = tf.truncated_normal_initializer(mean=0, stddev=1. / N_SIZE, dtype=tf.float32)
+        self.W1 = tf.get_variable("W1", [N_SIZE * N_SIZE, h], initializer=xavier_l1)
+        xavier_l2 = tf.truncated_normal_initializer(mean=0, stddev=1. / np.sqrt(h), dtype=tf.float32)
+        self.W2 = tf.get_variable("W2", [h, n_actions], initializer=xavier_l2)
+
+        # Build Computation
+        # tf reward processing (need tf_discounted_epr for policy gradient wizardry)
+        tf_discounted_epr = self.tf_discount_rewards(self.tf_epr)
+        tf_mean, tf_variance = tf.nn.moments(tf_discounted_epr, [0], shift=None, name="reward_moments")
+        tf_discounted_epr -= tf_mean
+        tf_discounted_epr /= tf.sqrt(tf_variance + 1e-6)
+
+        # Define Optimizer, compute and apply gradients
+        self.tf_aprob = self.tf_policy_forward(self.tf_x)
+        loss = tf.nn.l2_loss(self.tf_y - self.tf_aprob)
+        optimizer = tf.train.RMSPropOptimizer(eta, decay=decay)
+        tf_grads = optimizer.compute_gradients(loss, var_list=tf.trainable_variables(), grad_loss=tf_discounted_epr)
+        self.train_op = optimizer.apply_gradients(tf_grads)
+
+        # Initialize Variable
+        init = tf.global_variables_initializer()
+
+        # initiates an interactive TensorFlow session
+        self.session = tf.InteractiveSession()
+        self.session.run(init)
+        self.load()
+
+    def tf_discount_rewards(self, tf_r):  # tf_r ~ [game_steps,1]
+        discount_f = lambda a, v: a * self.gamma + v;
+        tf_r_reverse = tf.scan(discount_f, tf.reverse(tf_r, [0]))
+        tf_discounted_r = tf.reverse(tf_r_reverse, [0])
+        return tf_discounted_r
+
+    def tf_policy_forward(self, x): #x ~ [1,D]
+        # a two-layer NN: to provide the probability of moving the paddle UP, given an input observation state
+        # The network takes the processed image of the state of the game and
+        # generates a single number denoting the probability of moving the paddle UP.
+        h = tf.matmul(x, self.W1)
+        h = tf.nn.relu(h)
+        logp = tf.matmul(h, self.W2)
+        p = tf.nn.softmax(logp)
+        return p
+
+    def predict_UP(self,x):
+        # to compute the probability:
+        # because In TensorFlow, the network graph is computed only in the TensorFlow session,
+        feed = {self.tf_x: np.reshape(x, (1, -1))}
+        aprob = self.session.run(self.tf_aprob, feed);
+        return aprob
+
+    def update(self, feed):
+        return self.session.run(self.train_op, feed)
+
+    def load(self):
+        self.saver = tf.train.Saver(tf.global_variables())
+        load_was_success = True  # yes, I'm being optimistic
+        try:
+            save_dir = '/'.join(self.save_path.split('/')[:-1])
+            ckpt = tf.train.get_checkpoint_state(save_dir)
+            load_path = ckpt.model_checkpoint_path
+            self.saver.restore(self.session, load_path)
+        except:
+            print("no saved model to load. starting new session")
+            load_was_success = False
+        else:
+            print("loaded model: {}".format(load_path))
+            saver = tf.train.Saver(tf.global_variables())
+            episode_number = int(load_path.split('-')[-1])
+
+    def save(self):
+        self.saver.save(self.session, self.save_path, global_step=n)
+        print("SAVED MODEL #{}".format(n))
+
+# downsampling
+def preprocess(I):
+    """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+    I = I[35:195] # crop
+    I = I[::2,::2,0] # downsample by factor of 2
+    I[I == 144] = 0  # erase background (background type 1)
+    I[I == 109] = 0  # erase background (background type 2)
+    I[I != 0] = 1    # everything else (paddles, ball) just set to 1
+    return I.astype(np.float).ravel()
+
+if __name__ == '__main__':
+    main()
